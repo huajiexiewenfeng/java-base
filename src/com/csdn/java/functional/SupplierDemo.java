26b1bf7ad2653f86a1ded5fd5bc7e97f68267ad2
xiewenfeng <xiewenfeng@megvii.com>
2020-11-06 18:38:48 +0800

    Supplier 示例改造

--- src/com/csdn/java/functional/SupplierDemo.java
+++ src/com/csdn/java/functional/SupplierDemo.java
@@ -2,24 +2,33 @@ package com.csdn.java.functional;
 
 import java.util.function.Supplier;
 
+/**
+ * @author xiewenfeng
+ */
 public class SupplierDemo {
-    public static void main(String[] args) {
-        Supplier<Long> supplier = getLong();
-        System.out.println(supplier.get());
-    }
 
-    public static Supplier<Long> getLong(){
-        return ()->{
-            return System.currentTimeMillis();
-        };
-    }
+  public static void main(String[] args) {
+    Supplier<Long> supplier = getLong();
+    System.out.println(supplier.get());
+  }
 
-    /**
-     * 第二种写法
-     * @return
-     */
-    public static Supplier<Long> getLongOther(){
-        return System::currentTimeMillis;
+  /**
+   * 第一种写法
+   *
+   * @return
+   */
+  public static Supplier<Long> getLong() {
+    return () -> {
+      return System.currentTimeMillis();
+    };
+  }
 
-    }
+  /**
+   * 第二种写法
+   *
+   * @return
+   */
+  public static Supplier<Long> getLongOther() {
+    return System::currentTimeMillis;
+  }
 }
--- src/com/csdn/java/functional/SupplierDesignDemo.java
+++ src/com/csdn/java/functional/SupplierDesignDemo.java
@@ -3,45 +3,62 @@ package com.csdn.java.functional;
 import java.util.function.Supplier;
 
 public class SupplierDesignDemo {
-    public static void main(String[] args) {
-        ehco("Hello,World");
 
-        ehco(() -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return "lazy-Hello,World";
-        });
+  public static void main(String[] args) {
+    level1();
+    level2();
+    level3();
+    level4();
+  }
 
-        ehco(SupplierDesignDemo::getMessage);
+  /**
+   * 一般写法
+   */
+  private static void level1() {
+    ehco("Hello,World");
+  }
 
-        getMessage();//及时执行
+  /**
+   * Supplier + lambda
+   */
+  private static void level2() {
+    Supplier<String> supplyMessage = () -> {
+      return "lambda-Hello,World";
+    };
+    ehco(supplyMessage.get());
+  }
 
-        Supplier<String> message = supplyMessage();//待执行状态
+  /**
+   * Supplier 作为 ehco 方法参数
+   */
+  private static void level3() {
+    ehco(SupplierDesignDemo::getMessage);
+  }
 
-        message.get();//实际执行
-    }
+  /**
+   * 执行关系
+   */
+  private static void level4() {
+    getMessage();//及时执行
+    Supplier<String> message = supplyMessage();//待执行状态
+    message.get();//实际执行
+  }
 
-    public static String getMessage() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "lazy-Hello,World";
-    }
+  public static void ehco(String message) {
+    System.out.println(message);
+  }
 
-    public static Supplier<String> supplyMessage() {
-        return SupplierDesignDemo::getMessage;
-    }
 
-    public static void ehco(String message) {
-        System.out.println(message);
-    }
+  public static String getMessage() {
+    return "lazy-Hello,World";
+  }
 
-    public static void ehco(Supplier<String> message) {
-        System.out.println(message.get());
-    }
+  public static Supplier<String> supplyMessage() {
+    return SupplierDesignDemo::getMessage;
+  }
+
+  public static void ehco(Supplier<String> message) {
+    System.out.println(message.get());
+  }
+
 }
